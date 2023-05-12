d2421cb0d68ca9d98d00bb5b2abe9383c06843a4
iparker3964 <iparker3964@yahoo.com>
2023-05-12 16:23:38 -0500

    Add project files.

---
+++ TheBugTrackerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheBugTrackerApp.Models;
+
+namespace TheBugTrackerApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ TheBugTrackerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheBugTrackerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
